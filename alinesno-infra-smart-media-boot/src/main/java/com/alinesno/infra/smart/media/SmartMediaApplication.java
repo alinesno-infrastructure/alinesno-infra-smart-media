24c53c6e273570ae90e4929ff26a50dc2e762df8
CarrotSea233 <2733614757@qq.com>
2023-10-04 09:33:59 +0800

    feat: 1.集成UI，2.集成SSO认证

--- alinesno-infra-smart-media-boot/src/main/java/com/alinesno/infra/smart/media/SmartMediaApplication.java
+++ alinesno-infra-smart-media-boot/src/main/java/com/alinesno/infra/smart/media/SmartMediaApplication.java
@@ -1,14 +1,15 @@
 package com.alinesno.infra.smart.media;
 
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 集成一个Java开发示例工具
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableInfraSsoApi
 @SpringBootApplication
 public class SmartMediaApplication {
 
